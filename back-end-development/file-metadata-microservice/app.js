1df2151b55bef5b64276898badbf8d9cefa4520d
0x0501 <41354084+0x0501@users.noreply.github.com>
2023-07-03 11:15:34 +0800

    Initial repo

---
+++ back-end-development/file-metadata-microservice/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import "dotenv/config";
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+
+app.use(express.static(resolve(__dirname, "public")));
+
+app.listen(process.env['EXPRESS_PORT'], () => {
+    console.log(`Express is listening on ${process.env['EXPRESS_PORT']}`);
+})
